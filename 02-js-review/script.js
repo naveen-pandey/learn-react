80f8185ca0118840b71606a152125449eda02a8a
naveen-pandey <naveenpandey0502@gmail.com>
2024-09-11 13:34:35 +0530

    commented old fetch

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -320,11 +320,11 @@ booksAfterUpdate;
 */
 
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("naveen");
+// console.log("naveen");
 
 async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
