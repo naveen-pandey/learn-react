6183b23285db5f538a861d01cf66a006232e627e
naveen-pandey <naveenpandey0502@gmail.com>
2024-09-13 00:30:01 +0530

    added useState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
 	"Learn React ⚛️",
 	"Apply for jobs 💼",
@@ -5,22 +7,25 @@ const messages = [
 ];
 
 export default function App() {
-	const step = 1;
+	const [step, setStep] = useState(1); // useState returns an array with two elements, first one is the state and second one is a function to update the state.
+	// So we destructure it and give it two variables, step and setStep.
+  // useState is a hook in React. 'use' is a prefix used for React hooks.
+  // hooks are only called on the top level of a functional component and not inside any other function or block.
 
 	function handlePrevious() {
-		alert("Previous");
+		if (step > 1) setStep(step - 1);
 	}
 
 	function handleNext() {
-		alert("Next");
+		if (step < 3) setStep(step + 1);
 	}
 
 	return (
 		<div className="steps">
 			<div className="numbers">
-				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
-				<div className={`${step >= 2 ? "active" : ""}`}>2</div>
-				<div className={`${step >= 3 ? "active" : ""}`}>3</div>
+				<div className={step >= 1 ? "active" : ""}>1</div>
+				<div className={step >= 2 ? "active" : ""}>2</div>
+				<div className={step >= 3 ? "active" : ""}>3</div>
 			</div>
 
 			<p className="message">
@@ -29,14 +34,12 @@ export default function App() {
 			<div className="buttons">
 				<button
 					style={{ backgroundColor: "#7950f2", color: "#fff" }}
-					onClick={handlePrevious}
-          >
+					onClick={handlePrevious}>
 					Previous
 				</button>
 				<button
 					style={{ backgroundColor: "#7950f2", color: "#fff" }}
-					onClick={handleNext}
-          >
+					onClick={handleNext}>
 					Next
 				</button>
 			</div>
