dcef56d89b47cfab88026036051ea1d832bc772f
naveen-pandey <naveenpandey0502@gmail.com>
2024-09-16 16:11:45 +0530

    travel list

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+function Logo() {
+	return <h1> 🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your trip? 😍</h3>
+		</div>
+	);
+}
+
+function PackingList() {
+	return <div className="list">LIST</div>;
+}
+
+function Stats() {
+	return (
+		<footer>
+			<em>💼 You have X items on your list. and you already packed X (X%)</em>
+		</footer>
+	);
+}
