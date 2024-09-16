10bc67f06d48c1b76fd9fa85c1ef5b9e5a9d0d57
naveen-pandey <naveenpandey0502@gmail.com>
2024-09-16 14:53:49 +0530

    step counter

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -16,17 +16,52 @@ export default function App() {
 	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
-		if (step > 1) setStep((s) => s - 1); // update the state according to the current state using callback function. 
-    // It is a safe approach to update the state based on the previous state. It also avoids bugs caused by race conditions. It is also more secure as it prevents accidental modification of the state. 
-    // If you simply need to update the state with a new value and you do not rely on the previous state, you can use a direct value.
+		if (step > 1) setStep((s) => s - 1); // update the state according to the current state using callback function.
+		// It is a safe approach to update the state based on the previous state. It also avoids bugs caused by race conditions. It is also more secure as it prevents accidental modification of the state.
+		// If you simply need to update the state with a new value and you do not rely on the previous state, you can use a direct value.
 	}
 
 	function handleNext() {
 		if (step < 3) setStep((s) => s + 1);
 
-    // BAD PRACTICE
-    // test.name="Fred"
-    // setStep({name: "Fred"}) // When we are not setting the state based on current state then we can pass the value normally like here.
+		// BAD PRACTICE
+		// test.name="Fred"
+		// setStep({name: "Fred"}) // When we are not setting the state based on current state then we can pass the value normally like here.
+	}
+
+	function Counter() {
+		const [count, setCount] = useState(0);
+		const [step, setStep] = useState(1);
+
+		const date = new Date("june 21 2027");
+		date.setDate(date.getDate() + count);
+
+		return (
+			<div>
+				<div>
+					<button onClick={() => setStep((c) => c - 1)}>-</button>
+					<span>Step: {step}</span>
+					<button onClick={() => setStep((c) => c + 1)}>+</button>
+				</div>
+
+				<div>
+					<button onClick={() => setCount((c) => c - step)}>-</button>
+					<span>Count: {count}</span>
+					<button onClick={() => setCount((c) => c + step)}>+</button>
+				</div>
+
+				<p>
+					<span>
+						{count === 0
+							? "Today is "
+							: count > 0
+							? `${count} days from today is `
+							: `${Math.abs(count)} days ago was `}
+					</span>
+					<span>{date.toDateString()}</span>
+				</p>
+			</div>
+		);
 	}
 
 	return (
@@ -58,6 +93,13 @@ export default function App() {
 							onClick={handleNext}>
 							Next
 						</button>
+					</div>
+					<br />
+          <br />
+          <br />
+          <br />
+					<div>
+						<Counter />
 					</div>
 				</div>
 			)}
